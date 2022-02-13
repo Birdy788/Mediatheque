372d023a9b611e3ea1c8451952e8f000f1cdee9e
Yassine Amrani <62746812+Birdy788@users.noreply.github.com>
2022-02-13 01:13:19 +0100

    Add files via upload

---
+++ build/CMakeFiles/Projet.dir/DependInfo.cmake
@@ -0,0 +1,29 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/amrani/Documents/Projet/cd.cpp" "/home/amrani/Documents/Projet/build/CMakeFiles/Projet.dir/cd.cpp.o"
+  "/home/amrani/Documents/Projet/console.cpp" "/home/amrani/Documents/Projet/build/CMakeFiles/Projet.dir/console.cpp.o"
+  "/home/amrani/Documents/Projet/dvd.cpp" "/home/amrani/Documents/Projet/build/CMakeFiles/Projet.dir/dvd.cpp.o"
+  "/home/amrani/Documents/Projet/interface.cpp" "/home/amrani/Documents/Projet/build/CMakeFiles/Projet.dir/interface.cpp.o"
+  "/home/amrani/Documents/Projet/livre.cpp" "/home/amrani/Documents/Projet/build/CMakeFiles/Projet.dir/livre.cpp.o"
+  "/home/amrani/Documents/Projet/main.cpp" "/home/amrani/Documents/Projet/build/CMakeFiles/Projet.dir/main.cpp.o"
+  "/home/amrani/Documents/Projet/numerique.cpp" "/home/amrani/Documents/Projet/build/CMakeFiles/Projet.dir/numerique.cpp.o"
+  "/home/amrani/Documents/Projet/ressource.cpp" "/home/amrani/Documents/Projet/build/CMakeFiles/Projet.dir/ressource.cpp.o"
+  "/home/amrani/Documents/Projet/revue.cpp" "/home/amrani/Documents/Projet/build/CMakeFiles/Projet.dir/revue.cpp.o"
+  "/home/amrani/Documents/Projet/vhs.cpp" "/home/amrani/Documents/Projet/build/CMakeFiles/Projet.dir/vhs.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/Projet.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Projet.dir/cd.cpp.o"
+  "CMakeFiles/Projet.dir/console.cpp.o"
+  "CMakeFiles/Projet.dir/dvd.cpp.o"
+  "CMakeFiles/Projet.dir/interface.cpp.o"
+  "CMakeFiles/Projet.dir/livre.cpp.o"
+  "CMakeFiles/Projet.dir/main.cpp.o"
+  "CMakeFiles/Projet.dir/numerique.cpp.o"
+  "CMakeFiles/Projet.dir/ressource.cpp.o"
+  "CMakeFiles/Projet.dir/revue.cpp.o"
+  "CMakeFiles/Projet.dir/vhs.cpp.o"
+  "Projet"
+  "Projet.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Projet.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
